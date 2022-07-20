f9cc5109fdcaeeb4b9ee955af61996eb091aa4b5
Tahmid Tuhin <tahmid.tuhin.3@gmail.com>
2022-07-21 02:20:33 +0600

    CV added

---
+++ app/Http/Controllers/mainController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class mainController extends Controller
+{
+    // public function indexView(){  // view function
+    //     return view('index');
+    // }
+}
